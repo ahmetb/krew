2c3bcf5260be86e42f83a99f8e43012aa5f9b4a9
Ahmet Alp Balkan <ahmetb@google.com>
2019-06-19 16:52:04 -0700

    hack: move out install_kubectl_if_needed
    - install appropriate kubectl (goos/goarch)
    - save it to $GOPATH/bin since "export $PATH" won't work outside a particular
      script
    - lowercase naming for local variables
    
    Fixes #210.
    
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- hack/run-integration-tests.sh
+++ hack/run-integration-tests.sh
@@ -31,19 +31,6 @@ EOF
   exit 0
 fi
 
-install_kubectl_if_needed() {
-  if hash kubectl 2>/dev/null; then
-    echo 'using kubectl from the host system'
-  else
-    # install kubectl
-    local -r KUBECTL_VERSION='v1.14.2'
-    local -r KUBECTL_BINARY="$BINDIR/kubectl"
-    curl -fSsLo "$KUBECTL_BINARY" https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
-    chmod +x "$KUBECTL_BINARY"
-    export PATH="$BINDIR:$PATH"
-  fi
-}
-
 KREW_BINARY=$(readlink -f "${1:-$KREW_BINARY_DEFAULT}")  # needed for `kubectl krew` in tests
 if [[ ! -x "${KREW_BINARY}" ]]; then
   echo "Did not find $KREW_BINARY. You need to build krew before running the integration tests"
